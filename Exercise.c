4154f742b718f8c136cfc01d8a810ebceaeb4e75
G1E2O3R4GE <yacoubg037@gmail.com>
2024-02-06 22:34:44 +0300

    FIRST COMMIT

---
+++ Exercise.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    int c;
+    int sum;
+    printf("Enter first number");
+    scanf("%d" ,&a);
+    printf("Enter second number");
+    scanf("%d" ,& b);
+    printf("Enter third number");
+    scanf("%d" ,&c);
+    sum=a+b+c;
+    printf("Total=%d" ,sum);
+    return 0;
+    }
